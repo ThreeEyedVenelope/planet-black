110a25c4f7dc49a33173342c0d9f00c13732fbe7
Scott Little <scott.little@loop.colum.edu>
2020-05-30 18:19:02 -0500

    Add ladder climbing functionality
    Added:
    - OnTriggerStay2D(), OnTriggerExit2D(), OnCollisionStay2D(), OnCollisionExit2D() methods to PlayerMovementController
    - Added m_isGrounded to the PlayerMovementController

--- Planet Black/Assets/Scripts/PlayerMovementController.cs
+++ Planet Black/Assets/Scripts/PlayerMovementController.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -8,11 +9,16 @@ public class PlayerMovementController : MonoBehaviour
 {
     [SerializeField, Tooltip("How fast will the player accelerate?")]
     private float m_accelerationRate;
-
+    
     [SerializeField, Tooltip("The fastest the player will walk.")]
     private float m_maxWalkSpeed;
 
-    private float m_horizontalInput;
+    [SerializeField, Tooltip("The fastest the player will climb ladders.")]
+    private float m_maxClimbingSpeed;
+
+    private float m_horizontalInput, m_verticalInput;
+
+    private bool m_isGrounded;
 
     private Rigidbody2D m_playerRigidbody;
 
@@ -29,20 +35,46 @@ public class PlayerMovementController : MonoBehaviour
     void Update()
     {
         m_horizontalInput = Input.GetAxisRaw("Horizontal");
+        m_verticalInput = Input.GetAxisRaw("Vertical");
+
+        SetDefaultValues();
+    }
+
+    private void SetDefaultValues()
+    {
+        if (m_accelerationRate == 0.0f)
+        {
+            m_accelerationRate = 10.0f;
+        }
+
+        if (m_maxWalkSpeed == 0.0f)
+        {
+            m_maxWalkSpeed = 10.0f;
+        }
+
+        if (m_maxClimbingSpeed == 0.0f)
+        {
+            m_maxClimbingSpeed = 10.0f;
+        }
     }
 
     private void FixedUpdate()
     {
+        HorizontalMovementHandler();
+    }
+
+    /// <summary>
+    /// Acts on the movement input recieved from the player
+    /// </summary>
+    private void HorizontalMovementHandler()
+    {
         if (!Mathf.Approximately(m_horizontalInput, 0.0f))
         {
-            m_playerRigidbody.AddForce(Vector2.right * m_horizontalInput * m_accelerationRate);
+            m_playerRigidbody.AddForce(Vector2.right * m_horizontalInput * (m_maxWalkSpeed * m_accelerationRate));
 
             Vector2 clampedVelocity = m_playerRigidbody.velocity;
 
             clampedVelocity.x = Mathf.Clamp(m_playerRigidbody.velocity.x, -m_maxWalkSpeed, m_maxWalkSpeed);
-
-            //TODO: Edit this to apply the climbing mechanic
-            clampedVelocity.y = 0.0f;
 
             m_playerRigidbody.velocity = clampedVelocity;
 
@@ -56,13 +88,71 @@ public class PlayerMovementController : MonoBehaviour
                 m_playerRenderer.flipX = false;
             }
         }
-        else
+        else if (m_isGrounded && Mathf.Approximately(m_horizontalInput, 0.0f))
         {
             Vector2 stoppingVelocity = m_playerRigidbody.velocity;
 
             stoppingVelocity = new Vector2(0.0f, 0.0f);
 
             m_playerRigidbody.velocity = stoppingVelocity;
+        }
+        else if (Mathf.Approximately(m_horizontalInput, 0.0f))
+        {
+            Vector2 stoppingVelocity = m_playerRigidbody.velocity;
+
+            stoppingVelocity.x = 0.0f;
+
+            m_playerRigidbody.velocity = stoppingVelocity;
+        }
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.tag == "Ladder")
+        {
+            //TODO: Debug: Check for Ladder collision trigger
+            Debug.Log("The player has detected the ladder");
+
+            if (!Mathf.Approximately(m_verticalInput, 0.0f))
+            {
+                m_playerRigidbody.constraints = RigidbodyConstraints2D.FreezeRotation;
+
+                m_playerRigidbody.AddForce(Vector2.up * m_verticalInput * (m_maxClimbingSpeed * m_accelerationRate));
+
+                Vector2 clampedVelocity = m_playerRigidbody.velocity;
+
+                clampedVelocity.y = Mathf.Clamp(m_playerRigidbody.velocity.y, -m_maxClimbingSpeed, m_maxClimbingSpeed);
+
+                m_playerRigidbody.velocity = clampedVelocity;
+            }
+            else
+            {
+                m_playerRigidbody.constraints = RigidbodyConstraints2D.FreezePositionY | RigidbodyConstraints2D.FreezeRotation;
+            }
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.tag == "Ladder")
+        {
+            m_playerRigidbody.constraints = RigidbodyConstraints2D.FreezeRotation;
+        }
+    }
+
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Ground")
+        {
+            m_isGrounded = true;
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Ground")
+        {
+            m_isGrounded = false;
         }
     }
 }
