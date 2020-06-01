09b430f493bdc9daf1acd1f4e72a62b85029e5d9
Scott Little <scott.little@loop.colum.edu>
2020-06-01 00:34:33 -0500

    Add steam leak SFX and Begin work on pickups

---
+++ Planet Black/Assets/Scripts/PlayerInteractionHandler.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Experimental.AssetImporters;
+using UnityEngine;
+
+/// <summary>
+/// Handles interaction between the player and interactables like keycards and doors
+/// </summary>
+public class PlayerInteractionHandler : MonoBehaviour
+{
+    private bool m_hasToolBox = false;
+    private bool m_hasStorageRoomKeycard = false;
+    private bool m_hasBridgeKeycard = false;
+
+    private void OnTriggerEnter2D(Collider2D collision) // changing OnTriggerStay2D to OnTriggerEnter2D
+    {
+
+        // TODO: Debugging Pickup Interaction
+        Debug.Log($"The player has entered a triggerVolume.");
+
+        if (collision.tag == "Pickup")
+        {
+            // TODO: Add Pickup Interaction Input
+            //Debug.Log($"The player is trying to interact. PickupInput == {PickupInput}");
+
+            if (collision.GetComponent<PickupObject>().IsToolbox)
+            {
+                m_hasToolBox = true;
+            }
+            else if (collision.GetComponent<PickupObject>().IsStorageRoomKeycard)
+            {
+                m_hasStorageRoomKeycard = true;
+            }
+            else if (collision.GetComponent<PickupObject>().IsBridgeKeycard)
+            {
+                m_hasBridgeKeycard = true;
+            }
+
+            collision
+        }
+    }
+}
